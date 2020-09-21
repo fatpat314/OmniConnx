8ccabb9f2c62233628e244f1dee11b1d9df8e7fa
GSCrawley <gscrawley@gmail.com>
2020-09-20 17:59:31 -0700

    added temp stylesheet, will edit the hell out of it dont worry

--- OmniConnx/settings.py
+++ OmniConnx/settings.py
@@ -132,4 +132,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
+
 WIKI_PAGE_TITLE_MAX_LENGTH = 600
--- categorys/urls.py
+++ categorys/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
 from . import views
 from .views import New_wiki_form, PageDetailView  #PageListView,
+# from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from . import settings
+admin.autodiscover()
 
 # app_name = 'categorys'
 urlpatterns = [
@@ -9,10 +15,13 @@ urlpatterns = [
     path('listings/<str:parent_or_child>/<int:pk>/',views.listing_view, name='listing'),
     path('post/<int:pk>',PageDetailView.as_view(), name='post'),
     path('form/', New_wiki_form.as_view(), name='new'),
+]
+
+urlpatterns += staticfiles_urlpatterns()
 
     # path('', PageListView.as_view(), name='wiki-list-page'),
     # path('form/', New_wiki_form.as_view(), name='new'),
     # path('<str:slug>/', PageDetailView.as_view(), name='post'),
     # path('accounts/', include('django.contrib.auth.urls')),
     # path('form/', New_wiki_form.as_view(), name='new'),
-    ]
+    
