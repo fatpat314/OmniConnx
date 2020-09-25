5ad1ca2c9cdc60aa40aa6b36919b9ba98a0f9953
GSCrawley <gscrawley@gmail.com>
2020-09-25 14:56:27 -0700

    added animated logo to home page just to see if it works

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from accounts.views import SignUpView
+from . import views
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name = 'signup')
-]
+    path('signup/', SignUpView.as_view(), name = 'signup'),
+    # path('home/', HomeView.as_view(), name = 'home'),
+] 
