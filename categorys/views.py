e83c026631c2910d056eeb9fabed31478e0d4b7a
Patrick <w.patrick.kelly@gmail.com>
2020-10-15 17:22:04 -0700

    newest first

--- categorys/views.py
+++ categorys/views.py
@@ -81,18 +81,18 @@ def index_view(request, parent_or_child=None, pk=None):
     categories = Category.objects.filter(parent=None)
 
     if parent_or_child is None:
-        listings = Listing.objects.all()
+        listings = Listing.objects.all().order_by("-created")
 
     elif parent_or_child == 'child':
         sub_cat = SubCategory.objects.get(pk=pk)
-        listings = sub_cat.listing_set.all()
+        listings = sub_cat.listing_set.all().order_by("-created")
 
     elif parent_or_child == 'parent':
         listings = []
-        sub_cats = Category.objects.get(pk=pk).children.all()
+        sub_cats = Category.objects.get(pk=pk).children.all().order_by("-created")
 
         for sub_cat in sub_cats:
-            prds = sub_cat.listing_set.all()
+            prds = sub_cat.listing_set.all().order_by("-created")
             listings += prds
     else:
         listings = []
@@ -108,18 +108,20 @@ def listing_view(request, parent_or_child=None, pk=None):
     categories = Category.objects.filter(parent=None)
 
     if parent_or_child is None:
-        listings = Listing.objects.all()
+        listings = Listing.objects.all().order_by("-created")
 
     elif parent_or_child == 'child':
         sub_cat = SubCategory.objects.get(pk=pk)
-        listings = sub_cat.listing_set.all()
+        listings = sub_cat.listing_set.all().order_by("-created")
+
 
     elif parent_or_child == 'parent':
         listings = []
-        sub_cats = Category.objects.get(pk=pk).children.all()
+        sub_cats = Category.objects.get(pk=pk).children.all().order_by("-created")
+
 
         for sub_cat in sub_cats:
-            prds = sub_cat.listing_set.all()
+            prds = sub_cat.listing_set.all().order_by("-created")
             listings += prds
     else:
         listings = []
@@ -153,36 +155,36 @@ class PageDetailView(DetailView):
           'post': post
         })
 
-# class PageListView(ListView):
-#     """ Renders a list of all Pages. """
-#     model = Listing
-#
-#     def get(self, request, parent_or_child=None, pk=None):
-#         """ GET a list of Pages. """
-#         categories = Category.objects.filter(parent=None)
-#
-#         if parent_or_child is None:
-#             listings = Listing.objects.all()
-#
-#         elif parent_or_child == 'child':
-#             sub_cat = SubCategory.objects.get(pk=pk)
-#             listings = sub_cat.listing_set.all()
-#
-#         elif parent_or_child == 'parent':
-#             listings = []
-#             sub_cats = Category.objects.get(pk=pk).children.all()
-#
-#             for sub_cat in sub_cats:
-#                 prds = sub_cat.listing_set.all()
-#                 listings += prds
-#         else:
-#             listings = []
-#
-#         return render(
-#             request,
-#             'users/profile.html',
-#             {'categories': categories, 'listings': listings}
-#         )
+class PageListView(ListView):
+    """ Renders a list of all Pages. """
+    model = Listing
+
+    def get(self, request, parent_or_child=None, pk=None):
+        """ GET a list of Pages. """
+        categories = Category.objects.filter(parent=None)
+
+        if parent_or_child is None:
+            listings = Listing.objects.all()
+
+        elif parent_or_child == 'child':
+            sub_cat = SubCategory.objects.get(pk=pk)
+            listings = sub_cat.listing_set.all()
+
+        elif parent_or_child == 'parent':
+            listings = []
+            sub_cats = Category.objects.get(pk=pk).children.all()
+
+            for sub_cat in sub_cats:
+                prds = sub_cat.listing_set.all()
+                listings += prds
+        else:
+            listings = []
+
+        return render(
+            request,
+            'users/profile.html',
+            {'categories': categories, 'listings': listings}
+        )
 
 class New_wiki_form(CreateView):
     def get(self, request, *args, **kwargs):
