cbb14bb4da779363f367005afa152969a100a50e
Patrick <w.patrick.kelly@gmail.com>
2020-10-18 18:23:39 -0700

    comments

--- categorys/models.py
+++ categorys/models.py
@@ -67,3 +67,17 @@ class Listing(models.Model):
 
         # Call save on the superclass.
         return super(Listing, self).save(*args, **kwargs)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
