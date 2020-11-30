6be39d9e0e83d44c2a5d56acc9e8c5bd77bc5334
Patrick <w.patrick.kelly@gmail.com>
2020-11-29 22:17:18 -0800

    four columns but now we need to add images to categories

--- OmniConnx/urls.py
+++ OmniConnx/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
     path(r'comments/', include('django_comments_xtd.urls')),
     # path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path(r'^user/', include('users.urls')),
+    path(r'user/', include('users.urls')),
     path('messages/', include('message.urls')),
     # path(r'^messages/', include('django_messages.urls')),
 
