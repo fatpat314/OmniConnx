b0c3a3feb971c47dac9e3f698ba6863d697681c5
Patrick <w.patrick.kelly@gmail.com>
2020-12-03 22:28:26 -0800

    heroku test

--- OmniConnx/settings.py
+++ OmniConnx/settings.py
@@ -188,3 +188,7 @@ WIKI_PAGE_TITLE_MAX_LENGTH = 600
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # AWS_S3_SIGNATURE_VERSION = 's3v4'
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
